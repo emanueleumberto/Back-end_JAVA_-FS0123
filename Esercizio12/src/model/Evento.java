b68f2e5cf58975b7221a3298b8bb6528e3e9e0f8
emanueleumberto <info@emanueleumberto.it>
2023-05-17 18:12:56 +0200

    Soluzione esercizio Gestione Eventi

---
+++ Esercizio12/src/model/Evento.java
@@ -0,0 +1,84 @@
+package model;
+
+import java.time.LocalDate;
+
+import utils.TipoEvento;
+
+public class Evento {
+	
+	private Integer id;
+	private String titolo;
+	private LocalDate dataEvento;
+	private String descrizione;
+	private TipoEvento tipoEvento;
+	private Integer numeroMassimoPartecipanti;
+	
+	public Evento() {
+		super();
+	}
+
+	public Evento(String titolo, LocalDate dataEvento, String descrizione, TipoEvento tipoEvento,
+			Integer numeroMassimoPartecipanti) {
+		super();
+		this.titolo = titolo;
+		this.dataEvento = dataEvento;
+		this.descrizione = descrizione;
+		this.tipoEvento = tipoEvento;
+		this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getDataEvento() {
+		return dataEvento;
+	}
+
+	public void setDataEvento(LocalDate dataEvento) {
+		this.dataEvento = dataEvento;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public TipoEvento getTipoEvento() {
+		return tipoEvento;
+	}
+
+	public void setTipoEvento(TipoEvento tipoEvento) {
+		this.tipoEvento = tipoEvento;
+	}
+
+	public Integer getNumeroMassimoPartecipanti() {
+		return numeroMassimoPartecipanti;
+	}
+
+	public void setNumeroMassimoPartecipanti(Integer numeroMassimoPartecipanti) {
+		this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+	}
+
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", titolo=" + titolo + ", dataEvento=" + dataEvento + ", descrizione=" + descrizione
+				+ ", tipoEvento=" + tipoEvento + ", numeroMassimoPartecipanti=" + numeroMassimoPartecipanti + "]";
+	}
+
+}
