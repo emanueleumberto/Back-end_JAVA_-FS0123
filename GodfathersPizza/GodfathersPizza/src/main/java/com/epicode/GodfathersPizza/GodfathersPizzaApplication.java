67f6b17dc917fb0f750ce47abe6c5d996a891762
emanueleumberto <info@emanueleumberto.it>
2023-05-31 11:10:11 +0200

    Soluzione parziale esercizio

--- GodfathersPizza/GodfathersPizza/src/main/java/com/epicode/GodfathersPizza/GodfathersPizzaApplication.java
+++ GodfathersPizza/GodfathersPizza/src/main/java/com/epicode/GodfathersPizza/GodfathersPizzaApplication.java
@@ -12,28 +12,6 @@ public class GodfathersPizzaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GodfathersPizzaApplication.class, args);
-		
-		configWith_Bean();
 	}
 	
-	public static void configWith_Bean() {
-		// Creo un Container dove utilizzare i bean sulla base del file di configurazione ConfigurationTest creato
-		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(MenuConfiguration.class);
-		
-		// Recupero il Bean Menu
-		Menu menu = (Menu) appContext.getBean("menu");
-		System.out.println("******* Menu *******");
-		System.out.println("PIZZAS");
-		menu.getMenuPizza().forEach(p -> System.out.println(p.getMenuItemLine()));
-		System.out.println("TOPPINGS");
-		menu.getMenuTopping().forEach(t -> System.out.println(t.getMenuItemLine()));
-		System.out.println("DRINKS");
-		menu.getMenuDrink().forEach(d -> System.out.println(d.getMenuItemLine()));
-		System.out.println("FRANCHISE");
-		menu.getMenuFranchise().forEach(o -> System.out.println(o.getMenuItemLine()));
-		
-		// Chiudo il Context
-		appContext.close();
-	}
-
 }
